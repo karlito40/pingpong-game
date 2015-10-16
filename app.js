3b342230d5b6ba16cd6a3550ec34d0c4c1cb33bc
karlito <karlito40@gmail.com>
2015-10-16 18:13:56 +0200

    add redirection

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+app.get('/', function(req, res){
+  res.redirect('games/');
+});
 
 app.listen(process.env.PORT || 9999);
